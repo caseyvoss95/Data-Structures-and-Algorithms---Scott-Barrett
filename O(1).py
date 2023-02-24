e77fde99bd574eebc68bde704c8ea39ffecca2a0
caseyvoss95 <caseyvos@bu.edu>
2023-02-24 18:05:04 -0500

    Big O lecture materials

---
+++ O(1).py
@@ -0,0 +1,13 @@
+def add_items(n):
+    return n + n
+
+print(add_items(4))
+
+# This file is a code-along to a course lecture
+# I hand-typed each line of code, but it is NOT my own.
+
+# ALL CREDITS TO:
+# Data Structures & Algorithms - Python
+# by Scott Barrett
+# visit https://www.udemy.com/course/data-structures-algorithms-python/
+# to purchase this fabulous course!
---
+++ O(n^2).py
@@ -0,0 +1,15 @@
+def print_items(n):
+    for i in range(n):
+        for j in range(n):
+            print(i, j)
+
+print_items(10)
+
+# This file is a code-along to a course lecture
+# I hand-typed each line of code, but it is NOT my own.
+
+# ALL CREDITS TO:
+# Data Structures & Algorithms - Python
+# by Scott Barrett
+# visit https://www.udemy.com/course/data-structures-algorithms-python/
+# to purchase this fabulous course!
